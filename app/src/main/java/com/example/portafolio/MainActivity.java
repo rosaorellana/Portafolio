0193c15eee734884b5c1606482d51f694b8a4f12
Rosi <rosaorellana2306@gmail.com>
2020-09-16 17:07:22 -0600

    Calculadora

--- app/src/main/java/com/example/portafolio/MainActivity.java
+++ app/src/main/java/com/example/portafolio/MainActivity.java
@@ -7,67 +7,86 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TabHost;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-    direcciones_pais miDireccion = new direcciones_pais();
-
+    public Button btnCalcular;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TabHost tbhConversores = (TabHost) findViewById(R.id.tbhDirecciones);
-        tbhConversores.setup();
-
-        tbhConversores.addTab(tbhConversores.newTabSpec("Direccion").setContent(R.id.tabDireccion).setIndicator("DIRECCION", null));
-        tbhConversores.addTab(tbhConversores.newTabSpec("Pais").setContent(R.id.tabPais).setIndicator("PAIS", null));
-
-        final Spinner spnMun = (Spinner) findViewById(R.id.spnMun);
-
-        Spinner spnDepto = (Spinner) findViewById(R.id.spnDepto);
-        spnDepto.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
-                spnMun.setAdapter(new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_dropdown_item, miDireccion.obtenerMunicipio(position)));
-                Toast.makeText(getApplicationContext(), "Indice: " + position, Toast.LENGTH_LONG).show();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-
-            }
-        });
-
-        Button btnObtenerPais = (Button) findViewById(R.id.btnObtenerPais);
-        btnObtenerPais.setOnClickListener(new View.OnClickListener() {
+        btnCalcular = (Button)findViewById(R.id.btnCalcular);
+        btnCalcular.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Spinner spnPaises = (Spinner) findViewById(R.id.spnPaises);
-                spnPaises.setAdapter(new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_dropdown_item, miDireccion.obtenerPaises()));
+                procesar(view);
             }
         });
     }
-}
-class direcciones_pais {
-    String [][] mun = {
-            {"Seleccione un depto"},
-            {"Usulutan","Santa Maria", "Santa Elena", "Jiquilisco"},
-            {"El Transito", "San Jorge", "San Rafael"},
-            {"SRL","La Union","Anamoros","El Carmen"},
-            {""}
-    };
-    String[] paises = {"Guatemala","Honduras","El Salvador","Nicaragua","Costa Rica"};
+    public void procesar(View vista){
+        try {
+            RadioGroup optOperaciones = (RadioGroup) findViewById(R.id.optOperaciones);
+            TextView tempVal = (TextView) findViewById(R.id.txtnum1);
+            double num1 = Double.parseDouble(tempVal.getText().toString());
+            tempVal = (TextView) findViewById(R.id.txtnum2);
+            double num2 = Double.parseDouble(tempVal.getText().toString());
+            double respuesta = 0;
 
-    String[] obtenerMunicipio(int posicion){
-        return mun[posicion];
-    }
-    String[] obtenerPaises(){
-        return paises;
+
+            switch (optOperaciones.getCheckedRadioButtonId()) {
+                case R.id.optSuma:
+                    respuesta = num1 + num2;
+                    break;
+                case R.id.optResta:
+                    respuesta = num1 - num2;
+                    break;
+                case R.id.optMultiplicar:
+                    respuesta = num1 * num2;
+                    break;
+                case R.id.optDivision:
+                    respuesta = num1 / num2;
+                    break;
+                case R.id.optExponente:
+                    respuesta = Math.pow(num1, num2);
+                    break;
+                case R.id.optPorcentaje:
+                    respuesta = (num1 / num2) * num1;
+                    break;
+                case R.id.optModulo:
+                    respuesta = num1%num2;
+                    break;
+                case R.id.optFactoreo:
+                    respuesta = num1*(num1+1)*(Math.pow(num2,2)-3);
+                    break;
+            }
+
+            switch (optOperaciones.getCheckedRadioButtonId()) {
+                case 1: //suma
+                    respuesta = num1 + num2;
+                    break;
+                case 2: //resta
+                    respuesta = num1 - num2;
+                    break;
+                case 3: //multiplicacion
+                    respuesta = num1 * num2;
+                    break;
+                case 4: //division
+                    respuesta = num1 / num2;
+                    break;
+            }
+            tempVal = (TextView) findViewById(R.id.lblRespuesta);
+            tempVal.setText("Respuesta: " + respuesta);
+        }catch (Exception err){
+            TextView temp = (TextView) findViewById(R.id.lblRespuesta);
+            Toast.makeText(getApplicationContext(),"Por favor ingrese valores",Toast.LENGTH_LONG).show();
+        }
+
     }
 }
 
